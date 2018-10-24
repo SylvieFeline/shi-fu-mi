0c9acd18e13b364e2496efd78cc4fb419cf7c83a
Sylvie <dcl.sylvieg@18pixel.fr>
2018-10-24 14:34:08 +0200

    ajout version desktop

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var pierre = document.getElementById("pierre");
 var feuille = document.getElementById("feuille");
 var ciseaux = document.getElementById("ciseaux");
-var joueur = document.getElementById("joueur");
-var IA = document.getElementById("IA");
+var choixJoueur = document.getElementById("joueur");
+var choixIA = document.getElementById("IA");
 var resultat = document.getElementById("resultat");
-var partie = document.getElementById("partie");
+var initPartie = document.getElementById("partie");
 
 var img1 = "img/rock.jpg";
 var img2 = "img/paper.jpg";
@@ -25,67 +25,68 @@ function randomImg(){
 
 
 pierre.addEventListener("click",function(){
-    joueur.src = "img/rock.jpg";
+    choixJoueur.src = "img/rock.jpg";
     // IA.src = randomImg ();
     var rando = Math.floor(Math.random()*3);
     if (rando == 1){
-        IA.src = "img/rock.jpg";
+        choixIA.src = "img/rock.jpg";
         resultat.innerHTML = "Match nul";
         resultat.style.backgroundColor = "orange";
     }else if (rando == 2){
-        IA.src = "img/paper.jpg";
+        choixIA.src = "img/paper.jpg";
         resultat.innerHTML = "Vous avez perdu";
         resultat.style.backgroundColor = "red";
     } else {
-        IA.src = "img/sciss.jpg";
+        choixIA.src = "img/sciss.jpg";
         resultat.innerHTML = "Vous avez gagné";
         resultat.style.backgroundColor = "green";
     };
 });
 
 feuille.addEventListener("click",function(){
-    joueur.src = "img/paper.jpg";
+    choixJoueur.src = "img/paper.jpg";
     // IA.src = randomImg ();
     var rando = Math.floor(Math.random()*3);
     if (rando == 1){
-        IA.src = "img/paper.jpg";
+        choixIA.src = "img/paper.jpg";
         resultat.innerHTML = "Match nul";
         resultat.style.backgroundColor = "orange";
     }else if (rando == 2){
-        IA.src = "img/sciss.jpg";
+        choixIA.src = "img/sciss.jpg";
         resultat.innerHTML = "Vous avez perdu";
         resultat.style.backgroundColor = "red";
     } else {
-        IA.src = "img/rock.jpg";
+        choixIA.src = "img/rock.jpg";
         resultat.innerHTML = "Vous avez gagné";
         resultat.style.backgroundColor = "green";
     };
 });
 
 ciseaux.addEventListener("click",function(){
-    joueur.src = "img/sciss.jpg";
+    choixJoueur.src = "img/sciss.jpg";
     // IA.src = randomImg ();
     var rando = Math.floor(Math.random()*3);
     if (rando == 1){
-        IA.src = "img/sciss.jpg";
+        choixIA.src = "img/sciss.jpg";
         resultat.innerHTML = "Match nul";
         resultat.style.backgroundColor = "orange";
     }else if (rando == 2){
-        IA.src = "img/rock.jpg";
+        choixIA.src = "img/rock.jpg";
         resultat.innerHTML = "Vous avez perdu";
         resultat.style.backgroundColor = "red";
     } else {
-        IA.src = "img/paper.jpg";
+        choixIA.src = "img/paper.jpg";
         resultat.innerHTML = "Vous avez gagné";
         resultat.style.backgroundColor = "green";
     };
 });
 
 
-partie.addEventListener("click", function(){
-    joueur.src = "images/Portrait-icon.png";
-    IA.src ="images/IA.jpg";
-
+initPartie.addEventListener("click", function(){
+    choixJoueur.src = "images/Portrait-icon.png";
+    choixIA.src ="images/IA.jpg";
+    resultat.style.backgroundColor = " rgba(116, 81, 56, 0.4)";
+    resultat.innerHTML = "Prêt pour le combat ?";
 });
 
 
